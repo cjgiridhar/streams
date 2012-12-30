91b9a57e26ac8bfffd144b0295867f18910a2a4a
cjgiridhar <cjgiridhar@gmail.com>
2012-12-30 06:33:28 -0800

    patterns

--- pattern.js
+++ pattern.js
@@ -1,34 +1,47 @@
-// Arithematic progression
+// Apply patterns
 
 var pattern_ap = function(list) {
-	var flag = 1;
-	var a = list[0];
-	var d = list[1]-list[0];
-	for(var i=2; i< list.length; i++) {
-		num = parseInt(a) + i*d;
-  		if(list[i] != num) {
-   			flag = 0;
-   		}
-	}
-	return flag;
+        var flag = 1;
+        var a = list[0];
+        var d = list[1]-list[0];
+        for(var i=2; i< list.length; i++) {
+                num = parseInt(a) + i* parseInt(d);
+                if(list[i] != num) {
+                        flag = 0;
+                }
+        }
+        return flag;
 }
 
-// Fibonaci pattern
+var pattern_gp = function(list) {
+        var flag = 1;
+        var a = list[0];
+        var r = (list[1]/list[0]);
+        for(var i=2; i< list.length; i++) {
+                num = parseInt(a) * Math.pow(parseInt(r),i);
+                if(list[i] != num) {
+                        flag = 0;
+                }
+        }
+        return flag;
+}
+
+
 
 var pattern_fibonacci = function(list) {
-	var flag = 1;
-	for(var i=2; i< list.length; i++) {
-		if(list[i] != list[i-1] + list[i-2]){
-			flag = 0;
-		}
-	}
-	return flag;
+        var flag = 1;
+        for(var i=2; i< list.length; i++) {
+                if(parseInt(list[i]) != parseInt(list[i-1]) + parseInt(list[i-2])){
+                        flag = 0;
+                }
+        }
+        return flag;
 }
 
 // Pattern match on buffer
 var apply_patterns= function(list, callback) {
 		process.nextTick(function() {
-			callback( pattern_fibonacci(list) + pattern_ap(list) ); 
+			callback( pattern_fibonacci(list) + pattern_ap(list) + pattern_gp(list)); 
 		});
 }
 
