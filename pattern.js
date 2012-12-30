ffbd78a73cc857ab71d83e1a2884cf502b611a0c
cjgiridhar <cjgiridhar@gmail.com>
2012-12-29 23:15:24 -0800

    pattern analyzer

--- pattern.js
+++ pattern.js
@@ -1,4 +1,4 @@
-// Apply patterns
+// Arithematic progression
 
 var pattern_ap = function(list) {
 	var flag = 1;
@@ -13,35 +13,24 @@ var pattern_ap = function(list) {
 	return flag;
 }
 
+// Fibonaci pattern
+
 var pattern_fibonacci = function(list) {
 	var flag = 1;
-	var a = 5 * Math.pow(list[0],2) + 4;
-	var b = 5 * Math.pow(list[0],2) - 4;
-	if( (Math.sqrt(a) - Math.ceil(Math.sqrt(a)) == 0) || (Math.sqrt(b) - Math.ceil(Math.sqrt(b)) == 0) ) {
-		for(var i=2; i< list.length; i++) {
-			if(list[i] != list[i-1] + list[i-2]){
-				flag = 0;
-			}
+	for(var i=2; i< list.length; i++) {
+		if(list[i] != list[i-1] + list[i-2]){
+			flag = 0;
 		}
 	}
 	return flag;
 }
 
-patterns = [pattern_ap];
-//list = [1,2,3,4,6];
-//console.log(patterns.length);
-
-var apply_patterns = function(list) {
-	//console.log("list" + list);
-	for (var index=0; index < patterns.length; index++) {
-		//console.log(index + "pat" + patterns[index](list));
-  		if (patterns[index](list)) {
-			return 1;
-		}
-		else {
-			return 0;
-		}
-	}
+// Pattern match on buffer
+var apply_patterns= function(list, callback) {
+		process.nextTick(function() {
+			callback( pattern_fibonacci(list) + pattern_ap(list) ); 
+		});
 }
 
+// Export the method for import
 exports.apply_patterns = apply_patterns;
